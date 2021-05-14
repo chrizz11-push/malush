a06970854dcf6b280d19498a08a185139e73a7f8
chrizz11-push <christopherchima11@gmail.com>
2021-05-14 06:43:51 -0700

    first commit

---
+++ try.js
@@ -0,0 +1,98 @@
+//a = 5**53-1
+//console.log(a)
+//console.log(Number.MAX_VALUE)
+
+//console.log(Math.sqrt(25))
+
+//console.log(Math.pow(8,2))
+
+//console.log(Math.floor(4.9))
+
+ //let user_input = prompt("what is your name")
+ //let b = 49
+ //let d = 42
+ //let result = b + d
+ //alert(result)
+ //let head = (5+5) *3
+ //alert(head)
+
+ 
+
+//let _a= prompt("your first number")
+//let _b=prompt("second numer")
+//alert(_a)
+//alert(_b)
+//let _c=Number(_a) +Number (_b)
+//alert(_c)
+//let
+
+//let num = 9
+//num--
+//console.log(num)
+
+// let _num = 9
+// _num+=4
+// console.log(_num)
+
+
+
+
+//document.write(`welcome ${user_input}`)
+
+// console.log(6>4)
+// let a=5
+// let b=4
+// let c="5"
+// console.log(a===c)
+// console.log(typeof a==="string")
+// let a=6
+// let b=4
+// let result = (a>b)? "yes its true" : "no its not true"
+// console.log(result)
+
+// let man=true
+// let woman=false
+// let result = ()? "yes its true" : "no its not true"
+
+// number1="first number" 
+// console.log(number1)
+// hi_number= prompt(number1)
+// console.log(h1_number)
+console.log(3>4 || 4>3)
+a = 7
+b = "7"
+console.log(!(a===b))
+
+// functions are block of code that is design to 
+// function sum() {
+//     let a = 5
+//     let b = 7
+//     return result = a + b
+//     console.log(result)
+
+// }
+// console.log(sum())
+
+
+// OR EASY WAY
+// function sum() {
+//     let a = 5
+//     let b = 7
+    
+//     console.log(a + b)
+
+// }
+// sum()
+
+// other method
+// const add = () => {
+//     let a = 5
+//     let b = 7
+//     return a + b 
+// }
+// console.log(add())
+
+function add(a, b) {
+    return a + b
+}
+console.log(add(5, 8))
